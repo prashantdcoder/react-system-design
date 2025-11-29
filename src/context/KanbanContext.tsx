b0f6b74d47440e797040afaeac706aa75c87bc3a
Prashant Kumar <prashantdcoder@gmail.com>
2025-11-29 10:19:55 +0530

    update context logic and test case

--- src/context/KanbanContext.tsx
+++ src/context/KanbanContext.tsx
@@ -1,12 +1,12 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useCallback } from "react";
 import { generateUniqueId } from "../utils/appUtils";
-import { KanbanColumnItem, KanbanContextProp, KanbanProviderProps } from "../utils/types";
+import { KanbanColumn, KanbanColumnItem, KanbanContextProp, KanbanProviderProps } from "../utils/types";
 
 
 export const KanbanContext = createContext<KanbanContextProp>(null);
 
 const KanbanProvider = ({ children, title, columns: initialColumns }: KanbanProviderProps) => {
-    const [columns, setColumns] = useState([...initialColumns]);
+    const [columns, setColumns] = useState(initialColumns);
     const sourceRef = React.useRef<string>('');
     const destinationRef = React.useRef<string>('');
     const moveTaskIdRef = React.useRef<string>('');
@@ -16,42 +16,57 @@ const KanbanProvider = ({ children, title, columns: initialColumns }: KanbanProv
         sourceRef.current = sourceId;
     };
 
-    const onDrop = (e: React.DragEvent<HTMLDivElement>, destinationId: string): void => {
-        destinationRef.current = destinationId;
-        moveTask();
-    };
 
-    const onDragOver = (e: React.DragEvent<HTMLDivElement>): void => {
-        e.preventDefault();
-    };
 
-    const moveTask = () => {
+    const resetRefs = (): void => {
+        moveTaskIdRef.current = '';
+        sourceRef.current = '';
+        destinationRef.current = '';
+    }
+
+    const moveTask = useCallback(() => {
         const sourceColumnId: string = sourceRef.current;
         const destinationColumnId: string = destinationRef.current;
         const taskId: string = moveTaskIdRef.current;
-        const extractItemFromSource: KanbanColumnItem = columns.find(col => col.id === sourceColumnId)?.items.find(item => item.id === taskId);
+
         if (sourceColumnId === destinationColumnId) {
             return;
-        }
-        const updatedColumns = columns.map((col) => {
+        };
+
+        const extractItemFromSource: KanbanColumnItem | undefined = columns.find(col => col.id === sourceColumnId)?.items.find(item => item.id === taskId);
+
+
+        const updatedColumns: KanbanColumn[] = columns.map(col => {
             if (col.id === sourceColumnId) {
                 return {
                     ...col,
                     items: col.items.filter(item => item.id !== taskId)
-                }
+                };
             }
-
             if (col.id === destinationColumnId) {
-                const newItems = col.items.concat({ ...extractItemFromSource, id: generateUniqueId() } as any);
+                const newItems = [...col.items, extractItemFromSource];
                 return {
                     ...col,
                     items: [...newItems],
                 }
             }
             return col;
-        })
+        });
         setColumns(updatedColumns);
-    };
+    }, [columns]);
+
+    const onDragOver = useCallback((e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+    }, []);
+
+    const onDrop = useCallback(
+        (e: React.DragEvent<HTMLDivElement>, destinationId: string) => {
+            destinationRef.current = destinationId;
+            moveTask();
+            resetRefs();
+        },
+        [moveTask]
+    );
 
     return (
         <KanbanContext.Provider value={{ title, onDragStart, onDrop, columns, onDragOver }}>
